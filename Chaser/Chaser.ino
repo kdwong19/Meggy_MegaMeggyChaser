477119b62d1a435b9154f351f12183114ba575f1
kdwong19 <kdwong19@punahou.edu>
2016-11-01 15:27:09 -1000

    Finished Player movement

--- Chaser/Chaser.ino
+++ Chaser/Chaser.ino
@@ -1,9 +1,54 @@
+#include <MeggyJrSimple.h>
+
+int px = 0
+int py = 0
+
 void setup() {
   // put your setup code here, to run once:
-
+ MeggyJrSimpleSetup(); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+Pmovement();
+DisplaySlate();
+ClearSlate();
+
 }
+
+void PMovement()
+{
+  CheckButtonsPress();
+  if (Button_Up)
+  {
+    py++;
+  }
+  if (Button_Down)
+  {
+    py = py - 1;
+  }
+  if (Button_Right)
+  {
+    px++;
+  }
+  if (Button_Left)
+  px = px - 1;
+  if (px > 7)
+  {
+    px = 0;
+  }
+  if (py > 7)
+  {
+    py = 0;
+  }
+  if (px < 0)
+  {
+    px = 7;
+  }
+  if (py < 0)
+  {
+    py = 7;
+  }
+}
+
